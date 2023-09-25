f98df005dd6e90507cad9d23c20cb3025ed3f588
yogendramaarisetty <43344815+yogendramaarisetty@users.noreply.github.com>
2023-09-25 11:27:57 +0530

    1 file modified, 1 added

--- src/App.js
+++ src/App.js
@@ -1,63 +1,9 @@
 import React, { useState, useEffect, useRef } from 'react';
 import Editor from '@monaco-editor/react';
 import './style.css';
-
+import CodeEditor from './CodeEditor';
 export default function App() {
-  const languages = ['python', 'c', 'cpp', 'java', 'csharp'];
-  const [language, setLanguage] = useState('python');
-  const [codes, setCodes] = useState(codesByLanguage);
-  const editorRef = useRef(null);
-
-  function handleEditorDidMount(editor, monaco) {
-    // here is the editor instance
-    // you can store it in `useRef` for further usage
-    editorRef.current = editor;
-  }
-
-  const handleLanguageChange = (e) => {
-    setLanguage(e.target.value);
-  };
-
-  const handleCodeChange = (newValue, e) => {
-    const updatedCodes = codes.slice();
-    updatedCodes[language]['code'] = newValue;
-    setCodes(updatedCodes);
-    localStorage.setItem('codes', JSON.stringify(updatedCodes));
-  };
-
-  const handleRunClick = () => {
-    // Run the code
-  };
-
-  const handleSubmitClick = () => {
-    // Submit the code
-  };
-
-  const options = {
-    language,
-  };
-  return (
-    <div>
-      <div className="toolbar">
-        <select value={language} onChange={handleLanguageChange}>
-          {languages.map((language) => (
-            <option key={language}>{language}</option>
-          ))}
-        </select>
-      </div>
-      <Editor
-        height="70vh"
-        language={language}
-        value={codes[language]['code']}
-        onChange={handleCodeChange}
-      />
-      ;
-      <div className="bar">
-        <button onClick={handleRunClick}>Run</button>
-        <button onClick={handleSubmitClick}>Submit</button>
-      </div>
-    </div>
-  );
+  <CodeEditor codesByLanguage={codesByLanguage} />;
 }
 
 const codesByLanguage = {
