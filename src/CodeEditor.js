7b63569cc6a5766dd2ea26be162332e5f8cab5af
yogendra maarisetty <43344815+yogendramaarisetty@users.noreply.github.com>
2023-09-25 13:09:06 +0530

    Dark mode for VScode

--- src/CodeEditor.js
+++ src/CodeEditor.js
@@ -1,18 +1,14 @@
 import React, { useState, useRef } from "react";
 import Editor from "@monaco-editor/react";
+import DarkModeToggle from "./DarkModeToggle";
 import "./style.css";
 
 export default function CodeEditor({ codesByLanguage }) {
   const languages = ["python", "c", "cpp", "java", "csharp"];
   const [language, setLanguage] = useState("python");
   const [codes, setCodes] = useState(codesByLanguage);
-  const editorRef = useRef(null);
-
-  function handleEditorDidMount(editor, monaco) {
-    // here is the editor instance
-    // you can store it in `useRef` for further usage
-    editorRef.current = editor;
-  }
+  const [theme, setTheme] = useState("vs-dark");
+  const [isDarkMode, setIsDarkMode] = useState(false);
 
   const handleLanguageChange = (e) => {
     setLanguage(e.target.value);
@@ -33,6 +29,12 @@ export default function CodeEditor({ codesByLanguage }) {
     // Submit the code
   };
 
+  const handleModeChange = () => {
+    console.log(isDarkMode);
+    setIsDarkMode(!isDarkMode);
+    setTheme(isDarkMode ? "vs-dark" : "vs");
+  };
+
   return (
     <div>
       <div className="toolbar">
@@ -41,14 +43,18 @@ export default function CodeEditor({ codesByLanguage }) {
             <option key={language}>{language}</option>
           ))}
         </select>
+        <DarkModeToggle
+          onChange={handleModeChange}
+          toggleText={isDarkMode ? "Light Mode" : "Dark Mode"}
+        />
       </div>
       <Editor
         height="70vh"
         language={language}
         value={codes[language]["code"]}
         onChange={handleCodeChange}
+        theme={theme}
       />
-      ;
       <div className="bar">
         <button onClick={handleRunClick}>Run</button>
         <button onClick={handleSubmitClick}>Submit</button>
