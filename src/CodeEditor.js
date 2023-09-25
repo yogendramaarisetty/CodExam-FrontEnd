68dd80d791c37250d9e9fe5861c82597fd9411aa
yogendra maarisetty <43344815+yogendramaarisetty@users.noreply.github.com>
2023-09-26 00:47:40 +0530

    Added Judge API service

--- src/CodeEditor.js
+++ src/CodeEditor.js
@@ -2,13 +2,21 @@ import React, { useState, useRef } from "react";
 import Editor from "@monaco-editor/react";
 import DarkModeToggle from "./DarkModeToggle";
 import "./style.css";
+import judgeService from "./service";
+import { css } from "@emotion/react";
+import { ClipLoader } from "react-spinners";
 
+const service = new judgeService(
+  "cf4305d31bmsh82518687544739ep179fd2jsnf6d65d6ee270"
+);
 export default function CodeEditor({ codesByLanguage }) {
   const languages = ["python", "c", "cpp", "java", "csharp"];
   const [language, setLanguage] = useState("python");
   const [codes, setCodes] = useState(codesByLanguage);
   const [theme, setTheme] = useState("vs-dark");
   const [isDarkMode, setIsDarkMode] = useState(false);
+  const [output, setOutput] = useState("");
+  const [isSubmitting, setIsSubmitting] = useState(false);
 
   const handleLanguageChange = (e) => {
     setLanguage(e.target.value);
@@ -23,6 +31,28 @@ export default function CodeEditor({ codesByLanguage }) {
 
   const handleRunClick = () => {
     // Run the code
+    setIsSubmitting(true);
+
+    service
+      .submitCode(languageIds[language], codes[language]["code"])
+      .then((data) => {
+        setIsSubmitting(false);
+        console.log(data);
+        var status = data.status.description;
+        if (status === "Accepted") {
+          setOutput(data.stdout);
+        } else {
+          if (status === "Compilation Error") {
+            setOutput(`${status}\n ${data.compile_output}`);
+          } else {
+            setOutput(`${status}\n ${data.stderr} \n ${data.stdout}`);
+          }
+        }
+        // var status = null;
+        // service.getSubmissionDetails(data["token"]).then((data) => {
+        //   console.log(data);
+        // });
+      });
   };
 
   const handleSubmitClick = () => {
@@ -56,9 +86,30 @@ export default function CodeEditor({ codesByLanguage }) {
         theme={theme}
       />
       <div className="bar">
-        <button onClick={handleRunClick}>Run</button>
-        <button onClick={handleSubmitClick}>Submit</button>
+        {isSubmitting ? (
+          <ClipLoader
+            color={"#123abc"} // Set the color you prefer
+            loading={isSubmitting}
+            size={35} // Set the size you prefer
+          />
+        ) : (
+          <>
+            <button onClick={handleRunClick}>Run</button>
+            <button onClick={handleSubmitClick}>Submit</button>
+          </>
+        )}{" "}
+      </div>
+      <div className="output">
+        <code>{output}</code>
       </div>
     </div>
   );
 }
+
+const languageIds = {
+  python: 71,
+  java: 91,
+  cpp: 54,
+  c: 50,
+  csharp: 51
+};
